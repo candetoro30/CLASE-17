c7ac365adf3b77440566a55a5bfd36770b5925b9
Candelaria Toro <candelariatorom@gmail.com>
2023-11-28 13:24:20 -0300

    Final de clase

--- Project/config/views.py
+++ Project/config/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.template import Context, Template
 
 def saludo(request):
     return HttpResponse("Hola Django - Coder")
@@ -13,4 +14,11 @@ def nombre(request, nombre: str, apellido: str):
     return HttpResponse(f"{apellido}, {nombre}: edad {edad}")
 
 
+def probando_template(request):
+    mi_html = open("./templates/template1.html")
+    mi_template = Template(mi_html.read())
+    mi_html.close()
+    mi_contexto = Context()
+    mi_documento = mi_template.render(mi_contexto)
+    return HttpResponse(mi_documento)
 
