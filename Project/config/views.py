fecd4a646677c647e3f0e21c41a133a3baa350cb
Candelaria Toro <candelariatorom@gmail.com>
2023-11-28 12:39:34 -0300

    Agregada vista nombre

--- Project/config/views.py
+++ Project/config/views.py
@@ -6,3 +6,11 @@ def saludo(request):
 def saludo_vista(request):
     return HttpResponse("<h1>Segunda vista</h2>")
     
+def nombre(request, nombre: str, apellido: str):
+    nombre = nombre.capitalize()
+    apellido = apellido.capitalize()
+    edad = input("Ingresa tu edad: ")
+    return HttpResponse(f"{apellido}, {nombre}: edad {edad}")
+
+
+
