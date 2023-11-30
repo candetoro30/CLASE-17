ce25d1d74444c3dd213ae7b2f523ecf4fb081bf7
Candelaria Toro <candelariatorom@gmail.com>
2023-11-30 17:46:18 -0300

    template

--- project/apps/producto/views.py
+++ project/apps/producto/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    contexto = {"app": "Aplicacion producto"}
+    return render(request, "producto/index.html", contexto)
+    
+
